daa7137000b6aca1140539f7bb9a425d6d4a68dc
hu <hunterwhite@hugejideMacBook-Pro.local>
2023-10-03 15:04:29 +0800

    test

--- src/app/api/auth/signin/page.tsx
+++ src/app/api/auth/signin/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
 export default function GET() {
-  notFound();
+  // notFound();
   return <div>sign in</div>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/chat");
+  // redirect("/chat");
   return <div>chat</div>;
 }
