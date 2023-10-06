b4b889ad1aea33b2a4a04ce725155b27b888949e
hu <hunterwhite@hugejideMacBook-Pro.local>
2023-10-06 16:31:43 +0800

    fix safari height not fit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full w-full">
       <head>
         <link rel="icon" href="/gpt_logo.png" sizes="any" />
       </head>
-      <body className={inter.className}>
+      <body className={`${inter.className} h-full w-full`}>
         <AuthProvider>{props.children}</AuthProvider>
       </body>
     </html>
