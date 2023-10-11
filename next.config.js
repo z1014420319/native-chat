7329ad8f3a9321834deb43fd5198ff8a310f795b
hu <hunterwhite@hugejideMacBook-Pro.local>
2023-10-12 00:17:55 +0800

    keep-alive

--- next.config.js
+++ next.config.js
@@ -9,6 +9,26 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/chat",
+        headers: [
+          {
+            key: "Connection",
+            value: "Keep-Alive",
+          },
+          {
+            key: "Keep-Alive",
+            value: "timeout=120, max=2000",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
+
+//Connection: Keep-Alive
+//Keep-Alive: timeout=5, max=1000
